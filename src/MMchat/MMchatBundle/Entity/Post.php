58735a1bb8b2616334e7b2733eae1a0e56e62d67
Urban Etter <uetter@gmail.com>
2011-09-23 23:15:59 +0200

    Output is linkifyd and nl2bred.

--- src/MMchat/MMchatBundle/Entity/Post.php
+++ src/MMchat/MMchatBundle/Entity/Post.php
@@ -12,103 +12,161 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Post
 {
-    /**
-     * @var integer $id
-     *
-     * @ORM\Column(name="id", type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    private $id;
+	/**
+	 * @var integer $id
+	 *
+	 * @ORM\Column(name="id", type="integer")
+	 * @ORM\Id
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+	private $id;
 
-    /**
-     * @var text $post
-     *
-     * @ORM\Column(name="post", type="text")
-     */
-    private $post;
+	/**
+	 * @var text $post
+	 *
+	 * @ORM\Column(name="post", type="text")
+	 */
+	private $post;
 
-    /**
-     * @var datetime $created_at
-     *
-     * @ORM\Column(name="created_at", type="datetime")
-     */
-    private $created_at;
+	/**
+	 * @var datetime $created_at
+	 *
+	 * @ORM\Column(name="created_at", type="datetime")
+	 */
+	private $created_at;
 
-    /**
-     * @var text $author
-     * 
-     * @ORM\Column(name="author", type="text")
-     */
-    protected $author;
+	/**
+	 * @var text $author
+	 *
+	 * @ORM\Column(name="author", type="text")
+	 */
+	protected $author;
 
-    /**
-     * Get id
-     *
-     * @return integer 
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
+	/**
+	 * Get id
+	 *
+	 * @return integer
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
 
-    /**
-     * Set post
-     *
-     * @param text $post
-     */
-    public function setPost($post)
-    {
-        $this->post = $post;
-    }
+	/**
+	 * Set post
+	 *
+	 * @param text $post
+	 */
+	public function setPost($post)
+	{
+		$this->post = $post;
+	}
 
-    /**
-     * Get post
-     *
-     * @return text 
-     */
-    public function getPost()
-    {
-        return $this->post;
-    }
+	/**
+	 * Get post
+	 *
+	 * @return text
+	 */
+	public function getPost()
+	{
+		$post = $this->post;
+		$post = strip_tags($post);
+		$post = nl2br($post);
+		$post = $this->linkify($post);
+		return $post;
+	}
 
-    /**
-     * Set created_at
-     *
-     * @param datetime $createdAt
-     */
-    public function setCreatedAt($createdAt)
-    {
-        $this->created_at = $createdAt;
-    }
+	/**
+	 * Set created_at
+	 *
+	 * @param datetime $createdAt
+	 */
+	public function setCreatedAt($createdAt)
+	{
+		$this->created_at = $createdAt;
+	}
 
-    /**
-     * Get created_at
-     *
-     * @return datetime 
-     */
-    public function getCreatedAt()
-    {
-        return $this->created_at;
-    }
+	/**
+	 * Get created_at
+	 *
+	 * @return datetime
+	 */
+	public function getCreatedAt()
+	{
+		return $this->created_at;
+	}
 
-    /**
-     * Set author
-     *
-     * @param string $author
-     */
-    public function setAuthor($author)
-    {
-        $this->author = $author;
-    }
+	/**
+	 * Set author
+	 *
+	 * @param string $author
+	 */
+	public function setAuthor($author)
+	{
+		$this->author = $author;
+	}
 
-    /**
-     * Get author
-     *
-     * @return text 
-     */
-    public function getAuthor()
-    {
-        return $this->author;
-    }
+	/**
+	 * Get author
+	 *
+	 * @return text
+	 */
+	public function getAuthor()
+	{
+		return $this->author;
+	}
+
+	/* File: linkify.php
+	 * Version: 20101010_1000
+	 * Copyright: (c) 2010 Jeff Roberson - http://jmrware.com
+	 * MIT License: http://www.opensource.org/licenses/mit-license.php
+	 *
+	 * Summary: This script linkifys http URLs on a page.
+	 *
+	 * Usage: See example page: linkify.html
+	 */
+	private function linkify($text) {
+		$url_pattern = '/# Rev:20100913_0900 github.com\/jmrware\/LinkifyURL
+# Match http & ftp URL that is not already linkified.
+# Alternative 1: URL delimited by (parentheses).
+(\() # $1 "(" start delimiter.
+((?:ht|f)tps?:\/\/[a-z0-9\-._~!$&\'()*+,;=:\/?#[\]@%]+) # $2: URL.
+(\)) # $3: ")" end delimiter.
+| # Alternative 2: URL delimited by [square brackets].
+(\[) # $4: "[" start delimiter.
+((?:ht|f)tps?:\/\/[a-z0-9\-._~!$&\'()*+,;=:\/?#[\]@%]+) # $5: URL.
+(\]) # $6: "]" end delimiter.
+| # Alternative 3: URL delimited by {curly braces}.
+(\{) # $7: "{" start delimiter.
+((?:ht|f)tps?:\/\/[a-z0-9\-._~!$&\'()*+,;=:\/?#[\]@%]+) # $8: URL.
+(\}) # $9: "}" end delimiter.
+| # Alternative 4: URL delimited by <angle brackets>.
+(<|&(?:lt|\#60|\#x3c);) # $10: "<" start delimiter (or HTML entity).
+((?:ht|f)tps?:\/\/[a-z0-9\-._~!$&\'()*+,;=:\/?#[\]@%]+) # $11: URL.
+(>|&(?:gt|\#62|\#x3e);) # $12: ">" end delimiter (or HTML entity).
+| # Alternative 5: URL not delimited by (), [], {} or <>.
+( # $13: Prefix proving URL not already linked.
+(?: ^ # Can be a beginning of line or string, or
+| [^=\s\'"\]] # a non-"=", non-quote, non-"]", followed by
+) \s*[\'"]? # optional whitespace and optional quote;
+| [^=\s]\s+ # or... a non-equals sign followed by whitespace.
+) # End $13. Non-prelinkified-proof prefix.
+( \b # $14: Other non-delimited URL.
+(?:ht|f)tps?:\/\/ # Required literal http, https, ftp or ftps prefix.
+[a-z0-9\-._~!$\'()*+,;=:\/?#[\]@%]+ # All URI chars except "&" (normal*).
+(?: # Either on a "&" or at the end of URI.
+(?! # Allow a "&" char only if not start of an...
+&(?:gt|\#0*62|\#x0*3e); # HTML ">" entity, or
+| &(?:amp|apos|quot|\#0*3[49]|\#x0*2[27]); # a [&\'"] entity if
+[.!&\',:?;]? # followed by optional punctuation then
+(?:[^a-z0-9\-._~!$&\'()*+,;=:\/?#[\]@%]|$) # a non-URI char or EOS.
+) & # If neg-assertion true, match "&" (special).
+[a-z0-9\-._~!$\'()*+,;=:\/?#[\]@%]* # More non-& URI chars (normal*).
+)* # Unroll-the-loop (special normal*)*.
+[a-z0-9\-_~$()*+=\/#[\]@%] # Last char can\'t be [.!&\',;:?]
+) # End $14. Other non-delimited URL.
+/imx';
+		$url_replace = '$1$4$7$10$13<a href="$2$5$8$11$14">$2$5$8$11$14</a>$3$6$9$12';
+		return preg_replace($url_pattern, $url_replace, $text);
+	}
 }
